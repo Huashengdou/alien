6659d19ac89603d5c5ea9639043087bb1281f884
yanjy <1019975412@qq.com>
2018-11-24 11:00:32 +0800

    创建外星人设置类，专门存储与外星人有关的设置

---
+++ settings.py
@@ -0,0 +1,11 @@
+
+class Settings():
+    """存储外星人入侵的所有设置类"""
+    def __init__(self):
+        """初始化游戏设置"""
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+
+        # 屏幕背景颜色设置
+        self.bg_color = (230, 230, 230)
