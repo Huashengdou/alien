772c094dcbbc79562d1363102f19bbfd6b20bea8
yanjy <1019975412@qq.com>
2018-11-24 10:46:09 +0800

    init the pygame module, create the game window

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,3 +1,21 @@
+import sys
+import pygame
 
+def run_game():
+    # 初始化游戏并创建一个屏幕对象
+    pygame.init()
 
-print("helllllllllll")
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    # 开始游戏主循环
+    while True:
+        # 监视鼠标和键盘事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
+run_game()
