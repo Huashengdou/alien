8abd0a71975c9454b2fec1e6071f76b6a678eba9
yanjy <1019975412@qq.com>
2018-11-25 15:04:40 +0800

    重构创建外星人函数

--- game_functions.py
+++ game_functions.py
@@ -49,19 +49,27 @@ def update_bullets(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
+def get_number_aliens_x(ai_settings, alien_width):
+    """计算每行可容纳多少个外星人"""
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+def create_alien(ai_settings, screen, aliens, alien_number):
+    """创建外星人"""
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2 * alien_width * alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
+
 def create_fleet(ai_settings, screen, aliens):
     """创建外星人群"""
     alien = Alien(ai_settings, screen)
-    alien_width = alien.rect.width
-
-    available_space_x = ai_settings.screen_width - 2*alien_width
-    number_aliens_x = int(available_space_x/(2*alien_width))
+    number_aliens_x = get_number_aliens_x(ai_settings, alien.rect.width)
     # 创建第一行外星人
     for alien_number in range(number_aliens_x):
-        alien = Alien(ai_settings, screen)
-        alien.x = alien_width + 2*alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+       create_alien(ai_settings, screen, aliens, alien_number)
 
 def update_screen(ai_settings, screen, ship, aliens, bullets):
     """更新屏幕图像，并切换到新屏幕"""
