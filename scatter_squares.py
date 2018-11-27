00f9e2d97f02f3bac5441dcd57e095c39bd45406
yanjy <1019975412@qq.com>
2018-11-27 22:33:55 +0800

    设置该点的显示方式

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,4 +1,12 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2,4)
+plt.scatter(2, 4, s=200)
+# 设置图表标题并给坐标轴加上标签
+plt.title("Square Hahaha", fontsize=24)
+plt.xlabel("Value", fontsize=24)
+plt.ylabel("Square", fontsize=24)
+
+# 设置刻度标记大小
+plt.tick_params(axis='both', which='major', labelsize=14)
+
 plt.show()
