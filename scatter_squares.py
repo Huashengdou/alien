e28d73b68eaa223c6a91f3a753b2749e25cf3cc4
yanjy <1019975412@qq.com>
2018-11-28 21:11:37 +0800

    删除数据点轮廓

--- scatter_squares.py
+++ scatter_squares.py
@@ -2,7 +2,8 @@ import matplotlib.pyplot as plt
 
 x_value = list(range(1,1001))
 y_value = [x ** 2 for x in x_value]
-plt.scatter(x_value, y_value, s=50)
+# edgecolors删除数据点轮廓，s是数据点大小
+plt.scatter(x_value, y_value, edgecolors='none', s=50)
 #plt.scatter(2, 4, s=200)
 # 设置图表标题并给坐标轴加上标签
 plt.title("Square Hahaha", fontsize=24)
