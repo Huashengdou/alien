a03aaf8fbb64678b878329bd3b8696a4683527cc
yanjy <1019975412@qq.com>
2018-11-28 21:08:52 +0800

    自动计算数据

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-x_value = [1, 2, 3, 4, 5]
-y_value = [1, 4, 9, 16, 25]
+x_value = list(range(1,1001))
+y_value = [x ** 2 for x in x_value]
 plt.scatter(x_value, y_value, s=50)
 #plt.scatter(2, 4, s=200)
 # 设置图表标题并给坐标轴加上标签
@@ -12,4 +12,6 @@ plt.ylabel("Square", fontsize=24)
 # 设置刻度标记大小
 plt.tick_params(axis='both', which='major', labelsize=14)
 
+# 设置每个坐标轴的取值范围
+plt.axis([10, 1100, 0, 1100000])
 plt.show()
