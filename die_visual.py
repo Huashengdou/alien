46600203c44e722bed0d25f58c91c91e5c931400
yanjy <1019975412@qq.com>
2018-11-30 22:10:40 +0800

    将次数以统计图可视化显示

--- die_visual.py
+++ die_visual.py
@@ -1,4 +1,5 @@
 from die import Die
+import pygal
 
 # 创建一个骰子
 die = Die()
@@ -14,4 +15,15 @@ frequencies = []
 for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
-print(frequencies)
+
+# 对结果进行可视化
+hist = pygal.Bar()
+hist.title = "Result of rolling one D6 1000 times"
+hist.x_labels = ['1','2','3','4','5','6']
+hist._x_title = "result"
+hist._y_title = "Frequency of result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
+
+#print(frequencies)
