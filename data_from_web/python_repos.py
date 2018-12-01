a0716f57de086869ebc473dba4dcfcc3f04f8139
yanjy <1019975412@qq.com>
2018-12-01 14:29:17 +0800

    获取仓库信息

--- data_from_web/python_repos.py
+++ data_from_web/python_repos.py
@@ -9,3 +9,8 @@ print("Start code:", r.status_code)
 # 将响应的api存入一个变量中
 response_dict = r.json()
 print(response_dict.keys())
+print("Total repositories:", response_dict['total_count'])
+# 获取有关仓库信息
+repo_dicts = response_dict['items']
+print('repositories returned:',len(repo_dicts))
+
