b6d5c9e4783923613bdfb7ffcc47228bf5431f63
yanjy <1019975412@qq.com>
2018-12-02 00:40:41 +0800

    将信息可视化为一个直方图

--- data_from_web/python_repos.py
+++ data_from_web/python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # 执行API调用，并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&stars'
@@ -8,13 +10,27 @@ print("Start code:", r.status_code)
 
 # 将响应的api存入一个变量中
 response_dict = r.json()
-print(response_dict.keys())
-print("Total repositories:", response_dict['total_count'])
+#print(response_dict.keys())
+#print("Total repositories:", response_dict['total_count'])
 # 获取有关仓库信息
 repo_dicts = response_dict['items']
-print('repositories returned:',len(repo_dicts))
+names, stars = [],[]
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+
+# 可视化
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(style=my_style, x_label=45, show_legend=False)
+chart.title = 'Most-Starred Python Project on Github'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svq')
+"""
 # 研究第一个仓库
 repo_dict = repo_dicts[0]
+
 #for key in sorted(repo_dict.keys()):
 #   print(key)
 
@@ -26,3 +42,4 @@ print('Repository:',repo_dict['html_url'])
 print('Created:',repo_dict['created_at'])
 print('Updated:',repo_dict['updated_at'])
 print('Description:',repo_dict['description'])
+"""
