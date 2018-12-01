8f0875cc4209d1e35fe52f4b410d9b96e456197e
yanjy <1019975412@qq.com>
2018-12-01 15:27:39 +0800

    去除空白行

--- study_python/file_reader.py
+++ study_python/file_reader.py
@@ -1,4 +1,6 @@
 
 with open('pi_digits.txt') as  file_object:
-    content = file_object.read()
-    print(content)
+    for line in file_object:
+        print(line.rstrip())
+    #content = file_object.read()
+    #print(content)
