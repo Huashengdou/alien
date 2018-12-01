fdd03d8d0a5eed51568ce37f9ed6a3b3e2e9844d
yanjy <1019975412@qq.com>
2018-12-01 15:34:16 +0800

    写入文件

--- study_python/file_reader.py
+++ study_python/file_reader.py
@@ -2,7 +2,15 @@
 with open('pi_digits.txt') as  file_object:
     lines = file_object.readlines()
 
+pi_string = ''
 for line in lines:
-    print(line.rstrip())
+    pi_string += line.strip()
+
+print(pi_string)
+print(len(pi_string))
+
+file_name = 'programming.txt'
+with open(file_name, 'w') as file_obj:
+    file_obj.write('l love python')
     #content = file_object.read()
     #print(content)
