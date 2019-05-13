7b6dfbba199011a8df1e33abaa12fe2547fa42d2
yanjy <1019975412@qq.com>
2019-05-13 21:41:25 +0800

    add thread note

--- games/halfPastTen/halfPastTen.py
+++ games/halfPastTen/halfPastTen.py
@@ -1,6 +1,22 @@
 
-playerName = input("请输入玩家姓名：")
+playerName = input("请输入第一个玩家姓名：")
 print("welcome "+ playerName)
+card1 = input("请输入第一张牌：")
+card2 = input("请输入第二张牌：")
+
+print("第一张牌为："+ card1)
+print("第二张牌为："+ card2)
+
+if card1[0] == "A":
+    card1_number = 1
+elif card1[0] == 'J' or card1[0] == "Q" or card1[0] == "K":
+    card1_number = 0.5
+elif len(card1) >= 2:
+    card1_number = 10
+else:
+    card1_number = int(card1[0])
+
+print(card1_number)
 money = input("请输入玩家筹码：")
 
 if int(money) < 0:
